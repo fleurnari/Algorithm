458c62db7258ece01606c121ef604c651d0a240d
fleurnari <132875045+fleurnari@users.noreply.github.com>
2025-08-11 09:36:39 +0900

    [Silver V] Title: 영화감독 숌, Time: 644 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+cnt = 0
+answer = 666
+while True:
+  if "666" in str(answer):
+    cnt += 1
+
+  if cnt == n:
+    break
+
+  answer += 1
+
+print(answer)
