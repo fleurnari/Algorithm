0c78f9eaad3ff3fcdab8261cbea6c751314e791f
fleurnari <132875045+fleurnari@users.noreply.github.com>
2025-09-13 09:27:27 +0900

    [Silver II] Title: 최소 힙, Time: 236 ms, Memory: 42600 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,15 @@
+import sys
+from queue import PriorityQueue
+input = sys.stdin.readline
+n = int(input())
+queue = PriorityQueue()
+
+for _ in range(n):
+  x = int(input())
+  if x == 0:
+    if queue.empty():
+      print(0)
+    else:
+      print(queue.get())
+  else:
+    queue.put(x)
