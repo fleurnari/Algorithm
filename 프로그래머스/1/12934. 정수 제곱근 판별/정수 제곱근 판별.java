94e67a881346a3eb7e3f6db27dd0305d3e96f789
fleurnari <132875045+fleurnari@users.noreply.github.com>
2025-09-18 10:17:04 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.09 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+        double sqrt = Math.sqrt(n);
+        
+        if (sqrt % 1 == 0) {
+            answer = (long)Math.pow(sqrt + 1, 2);
+        }
+        
+        
+        return answer;
+    }
+}
