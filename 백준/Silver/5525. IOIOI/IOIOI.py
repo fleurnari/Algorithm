9b3369a8a3e523d6ea8c1d9f30a899dd1c0b7367
fleurnari <132875045+fleurnari@users.noreply.github.com>
2025-09-26 21:20:41 +0900

    [Silver I] Title: IOIOI, Time: 364 ms, Memory: 35800 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n = int(input())
+m = int(input())
+s = input().rstrip()
+answer, i, cnt = 0, 0, 0
+
+while i < (m - 1):
+  if s[i:i + 3] == 'IOI':
+    i += 2
+    cnt += 1
+    if cnt == n:
+      answer += 1
+      cnt -= 1
+  else:
+    i += 1
+    cnt = 0
+
+print(answer)
