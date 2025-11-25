7c1f0fb1f5a7ace801f995868f715bbe0817ddee
fleurnari <132875045+fleurnari@users.noreply.github.com>
2025-11-25 13:59:09 +0900

    [Gold II] Title: 후위 표기식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+infix = input().strip()
+postfix = []
+stack = []
+
+for i in infix:
+  if i.isalpha():
+    postfix.append(i)
+  else:
+    if i == '(':
+      stack.append(i)
+    elif i == '*' or i == '/':
+      while stack and (stack[-1] == '*' or stack[-1] == '/'):
+        postfix.append(stack.pop())
+      stack.append(i)
+    elif i == '+' or i == '-':
+      while stack and stack[-1] != '(':
+        postfix.append(stack.pop())
+      stack.append(i)
+    elif i == ')':
+      while stack and stack[-1] != '(':
+        postfix.append(stack.pop())
+      stack.pop()
+
+while stack:
+  postfix.append(stack.pop())
+
+print(''.join(postfix))
