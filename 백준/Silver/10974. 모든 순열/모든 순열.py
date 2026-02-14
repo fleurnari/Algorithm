be2c72d68cb26d5054c50745ed630055fbb2cae2
fleurnari <132875045+fleurnari@users.noreply.github.com>
2026-02-14 17:01:26 +0900

    [Silver III] Title: 모든 순열, Time: 164 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+visited = [False] * (n + 1)
+path = []
+
+def dfs():
+  if len(path) == n:
+    print(*path)
+    return
+
+  for i in range(1, n + 1):
+    if not visited[i]:
+      visited[i] = True
+      path.append(i)
+      dfs()
+      path.pop()
+      visited[i] = False
+
+dfs()
