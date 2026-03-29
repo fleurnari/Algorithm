7d1ff334644df3155428880be2c1863f29f90b0e
fleurnari <132875045+fleurnari@users.noreply.github.com>
2026-03-30 06:52:09 +0900

    [Silver V] Title: 뒤집기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+s = input().rstrip()
+zero = 0
+one = 0
+
+if s[0] == '0':
+    zero += 1
+else:
+    one += 1
+
+for i in range(1, len(s)):
+    if s[i] != s[i - 1]:
+        if s[i] == '0':
+            zero += 1
+        else:
+            one += 1
+
+print(min(zero, one))
